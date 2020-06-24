cffd5fd7dc65d9a2ddc80e80f586ab28ff2be168
mahboobehjami-sh-a <mahboobehjami1380@gmail.com>
2020-06-24 19:06:25 +0430

    create showmovie activity

---
+++ app/src/main/java/com/example/popularmovie/ShowMovie.java
@@ -0,0 +1,14 @@
+package com.example.popularmovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowMovie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_movie);
+    }
+}
