1ff0f0e5e720512359792d17a619c9b57d0b3102
mahboobehjami-sh-a <mahboobehjami1380@gmail.com>
2020-06-25 11:09:55 +0430

    change ShowMovie.java

--- app/src/main/java/com/example/popularmovie/ShowMovie.java
+++ app/src/main/java/com/example/popularmovie/ShowMovie.java
@@ -1,88 +1,41 @@
 package com.example.popularmovie;
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class ShowMovie extends AppCompatActivity {
-    private ProgressBar progressBar;
-    private TextView tvname,tvdirector,tvstory;
-    private ImageView ivposter;
-    ShowMovie mHost ;
-    String path;
+    public ProgressBar progressBar;
+    public TextView tvname,tvdirector,tvstory;
+    public ImageView ivposter;
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    View root = inflater.inflate(R.layout.activity_show_movie, container, false);
-        tvname = root.findViewById(R.id.name);
-        tvdirector = root.findViewById(R.id.director);
-        tvstory = root.findViewById(R.id.story);
-        ivposter = root.findViewById(R.id.poster);
-        progressBar = root.findViewById(R.id.progress_circular_home);
-        final String urlposter = "https://image.tmdb.org/t/p/original/"+ path ;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        Glide.with(this).load(urlposter).into(ivposter);
+        progressBar = (ProgressBar) findViewById(R.id.progress_circular_home);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.GONE);
 
-        //Call Volley
-        getData();
-        return root;
-    }
+        ivposter = (ImageView) findViewById(R.id.poster);
+        String urlPoster=getIntent().getExtras().getString("");
+        Glide.with(this).load(urlPoster).into(ivposter);
 
+        tvname = (TextView) findViewById(R.id.name);
+        String urlName=getIntent().getExtras().getString("");
+        tvname.setText(urlName);
 
+        tvdirector = (TextView) findViewById(R.id.director);
+        String urlDirector=getIntent().getExtras().getString("");
+        tvdirector.setText(urlDirector);
 
-    private void getData() {
-     RequestQueue queue = Volley.newRequestQueue(getActivity());
-     String url = "https://api.themoviedb.org/3/movie/popular?api_key=2b6180dd56ad2250ddaf4ebf328fec1e" ;
-
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                progressBar.setVisibility(View.GONE);
-
-                try {
-                    JSONObject jsonObject = new JSONObject(response.toString());
-
-                    tvname.setText(jsonObject.getString("original_title"));
-                    tvdirector.setText(jsonObject.getString("charecter"));
-                    tvstory.setText(jsonObject.getString("title"));
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                progressBar.setVisibility(View.GONE);
-                Log.d( "Error Response", error.toString());
-            }
-        });
-
-        queue.add(stringRequest);
-    }
-
-    @Nullable
-    final public ShowMovie getActivity() {
-        return mHost == null ? null : (ShowMovie) mHost.getActivity();
+        tvstory = (TextView) findViewById(R.id.story);
+        String urlStory=getIntent().getExtras().getString("");
+        tvstory.setText(urlStory);
     }
 }
+
 
